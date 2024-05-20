ff94d86f027a39089fd55f2e15e67c320172554d
John <john.hs.hakansson@outlook.com>
2024-05-20 09:57:44 +0200

    Initial Scrape, get categories

---
+++ ProjectWebScraping/Models/ScrapedBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWebScraping.Models
+{
+  public class ScrapedBook
+  {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public decimal PriceWithoutTax { get; set; }
+    public decimal Tax { get; set; }
+    public int StarRating { get; set; }
+    public decimal AvailableInStock { get; set; }
+    public string UPC {  get; set; } = string.Empty;
+    public string ProductType { get; set; } = string.Empty;
+    public string NumberOfReviews { get; set; } = string.Empty;
+    public string Category {  get; set; } = string.Empty;
+  }
+}
---
+++ ProjectWebScraping/WebScraper.cs
@@ -0,0 +1,42 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWebScraping
+{
+  public class WebScraper
+  {
+    private readonly string _baseUrl = "https://books.toscrape.com/";
+
+    public void Scrape()
+    {
+      var web = new HtmlWeb();
+      HtmlDocument doc = web.Load(_baseUrl);
+
+      HtmlNode sidePanel = doc.DocumentNode.Descendants("div").Where(x => x.HasClass("side_categories")).First();
+
+      if(sidePanel == null)
+      {
+        return;
+      }
+
+      List<HtmlNode> categories = sidePanel.Descendants("ul")
+        .Where(node => !node.HasClass("nav nav-list"))
+        .First()
+        .Descendants("ul")
+        .First()
+        .Descendants("li")
+        .Select(li => li.SelectSingleNode("a"))
+        .ToList();
+        
+      foreach(var category in categories)
+      {
+        var link = category.GetAttributeValue("href", string.Empty);
+        Console.WriteLine(string.Format("{0}{1}", _baseUrl, link));
+      }
+    }
+  }
+}
